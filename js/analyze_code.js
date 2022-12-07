a8359f27f550b768027973fe02f380fb40fed500
Tolstosheyev Roman <izobretatel777@mail.ru>
2022-12-07 17:26:07 +0400

    Improved code formatting

--- js/analyze_code.js
+++ js/analyze_code.js
@@ -2,7 +2,10 @@ var badcode = `
 class NEFC {
     static void main(String[] args) {
         
-        List <Integer> a = new Arrays.asList(1,2,3,4,5,6,7);
+        List <Integer> a = new Arrays.asList(
+            1, 2, 3, 4, 5, 6, 7
+        );
+        
         List <Integer> b = new ArrayList<Integer>();
         
         for(Integer i : a) {
@@ -19,9 +22,13 @@ var goodcode = `
 class NEFC {
     static void main(String[] args) {
 
-        List <Integer> a = new Arrays.asList(1,2,3,4,5,6,7);
+        List <Integer> a = new Arrays.asList(
+            1, 2, 3, 4, 5, 6, 7
+        );
 
-        List <Integer> b = a.stream().filter(i -> i % 2 == 0).collect(Collectors.toList());
+        List <Integer> b = a.stream()
+                                .filter(i -> i % 2 == 0)
+                                .collect(Collectors.toList());
 
     }
 }
