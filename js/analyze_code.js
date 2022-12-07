2eaeca3d93eed5fe0662f6846adb3eda4b18e361
Tolstosheyev Roman <izobretatel777@mail.ru>
2022-12-07 17:13:07 +0400

    hardcoded code

---
+++ js/analyze_code.js
@@ -0,0 +1,37 @@
+var badcode = `
+class NEFC {
+    static void main(String[] args) {
+        
+        List <Integer> a = new Arrays.asList(1,2,3,4,5,6,7);
+        List <Integer> b = new ArrayList<Integer>();
+        
+        for(Integer i : a) {
+            if(i % == 0) {
+                b.add(i);
+            }
+        }
+    }
+}
+`;
+
+
+var goodcode = `
+class NEFC {
+    static void main(String[] args) {
+
+        List <Integer> a = new Arrays.asList(1,2,3,4,5,6,7);
+
+        List <Integer> b = a.stream().filter(i -> i % 2 == 0).collect(Collectors.toList());
+
+    }
+}
+`;
+
+
+function analyzeCode() {
+    var editor = ace.edit("editor");
+    code = editor.getValue();
+    if (code.replace(/\s/g, '') == badcode.replace(/\s/g, '')) {
+        editor.setValue(goodcode);
+    }
+}
